d0ea951b3644ca541f93d4ed75aad5afcae05718
Rae Hohle <rhohle@macalester.edu>
2016-02-23 21:55:31 -0600

    Added status to printers

--- FixItMac/www/js/services.js
+++ FixItMac/www/js/services.js
@@ -2,29 +2,37 @@
  * Created by codymolho on 2/23/16. And made spectacular by Rae, Minju, and Ana!
  */
 
+/*
+Printers four properties: name, location, status (true if working/false if not working)
+ */
 angular.module('starter.services', [])
 
 .factory('Printers', function() {
   var printers = [{
     id: 0,
     name: "Dupre",
-    location: "Dupre Hall 253"
+    location: "Dupre Hall 253",
+    status: true
   }, {
     id: 2,
     name: "Campus Center",
-    location: "Campus Center 2nd Floor"
+    location: "Campus Center 2nd Floor",
+    status: true
   }, {
     id: 3,
     name: "Kirk 1",
-    location: "Kirk Hall 012"
+    location: "Kirk Hall 012",
+    status: false
   }, {
     id: 4,
     name: "Kirk 2",
-    location: "Kirk Hall 012"
+    location: "Kirk Hall 012",
+    status: false
   }, {
     id: 5,
     name: "Doty",
-    location: "Doty Hall 004"
+    location: "Doty Hall 004",
+    status: true
   }];
 
   return {
