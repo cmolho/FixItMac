4abdb20c1896cd29a0adbbc846b32eea3888b786
cmolho <cmolho@macalester.edu>
2016-02-23 19:48:56 -0600

    Added to main page, added main controller and linked with test data

--- FixItMac/www/js/services.js
+++ FixItMac/www/js/services.js
@@ -1,3 +1,24 @@
 /**
  * Created by codymolho on 2/23/16.
  */
+
+angular.module('starter.services', [])
+
+.factory('Printers', function() {
+  var printers = [{
+    id: 0,
+    name: "Dupre",
+    location: "Dupre"
+  },{
+    id: 1,
+    name: "Lib",
+    location: "Lib"
+  }];
+
+  return {
+    all: function() {
+      return printers;
+    }
+  };
+
+});
