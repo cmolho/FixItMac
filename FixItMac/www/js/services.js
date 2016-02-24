7ba9f421ca7620988533676aaffe9250c4b917ce
Rae Hohle <rhohle@macalester.edu>
2016-02-23 20:28:37 -0600

    Added locations to printers. Took off test lib printer.

--- FixItMac/www/js/services.js
+++ FixItMac/www/js/services.js
@@ -8,27 +8,23 @@ angular.module('starter.services', [])
   var printers = [{
     id: 0,
     name: "Dupre",
-    location: "Dupre"
-  },{
-    id: 1,
-    name: "Lib",
-    location: "Lib"
+    location: "Dupre Hall 253"
   }, {
     id: 2,
     name: "Campus Center",
-    location: "Campus Center"
+    location: "Campus Center 2nd Floor"
   }, {
     id: 3,
     name: "Kirk 1",
-    location: "Kirk"
+    location: "Kirk Hall 012"
   }, {
     id: 4,
     name: "Kirk 2",
-    location: "Kirk"
+    location: "Kirk Hall 012"
   }, {
-    id: 4,
+    id: 5,
     name: "Doty",
-    location: "Doty"
+    location: "Doty Hall 004"
   }];
 
   return {
