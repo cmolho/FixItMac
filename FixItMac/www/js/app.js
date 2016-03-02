e92388bd097aad1af1d1f02a5bdfe50eb2267737
Rae Hohle <rhohle@macalester.edu>
2016-03-02 15:15:45 -0600

    Changed printer image to refer to saved image in folder instead of link. Created Reward template for reward page after issue is reported. Do not yet have linked to confirm report issue button yet.

--- FixItMac/www/js/app.js
+++ FixItMac/www/js/app.js
@@ -5,39 +5,46 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider
-
-    .state('main', {
-      url: '/main',
-      templateUrl: 'templates/main.html',
-      controller: 'MainCtrl'
-    })
-
-    .state('profile', {
-      url: '/profile/:id',
-      templateUrl: 'templates/profile.html',
-      controller: 'ProfileCtrl'
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
     });
+  })
 
-  $urlRouterProvider.otherwise('/main');
-});
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+
+      .state('main', {
+        url: '/main',
+        templateUrl: 'templates/main.html',
+        controller: 'MainCtrl'
+      })
+
+      .state('profile', {
+        url: '/profile/:id',
+        templateUrl: 'templates/profile.html',
+        controller: 'ProfileCtrl'
+      })
+
+      .state('reward', {
+        url:'/reward/:id',
+        templateUrl: 'templates/main.html',
+        controller: 'RewardCtrl'
+
+      });
+
+    $urlRouterProvider.otherwise('/main');
+  });
--- FixItMac/www/js/controllers.js
+++ FixItMac/www/js/controllers.js
@@ -4,26 +4,34 @@
 
 angular.module('starter.controllers', [])
 
-.controller('MainCtrl', function($scope, $state, $stateParams, Printers) {
-  $scope.printers = Printers.all()
-})
+  .controller('MainCtrl', function($scope, $state, $stateParams, Printers) {
+    $scope.printers = Printers.all()
+  })
 
-.controller('ProfileCtrl', function($scope, $stateParams, Printers, $ionicPopup) {
-  $scope.printer = Printers.get($stateParams.id),
-    $scope.showConfirm = function() {
+  .controller('ProfileCtrl', function($scope, $stateParams, Printers, $ionicPopup, $state) {
+    $scope.printer = Printers.get($stateParams.id),
+      $scope.showConfirm = function() {
 
-    var confirmPopup = $ionicPopup.confirm({
-      title: 'Are you sure?',
-      template: 'An automatic email will be generated to ITS if you report.'
-    });
+        var confirmPopup = $ionicPopup.confirm({
+          title: 'Are you sure?',
+          template: 'An automatic email will be generated to ITS if you report.',
+          cancelText:'Cancel',
+          okText: 'Yes, send the report!'
+        });
 
-    confirmPopup.then(function(res) {
-      if(res) {
-        console.log('Sure!');
-      } else {
-        console.log('Not sure!');
-      }
-    });
+        confirmPopup.then(function(result) {
+          if(result) {
+            //href="#/reward/{{printer.id}}"
+            $state.go('#/reward/{{printer.id}}');
+          } else {
+            //href="#/profile/{{printer.id}}"
+            $state.go('#/profile/{{printer.id}}');
+          }
+        });
 
-  };
-});
+      };
+  })
+  .controller('RewardCtrl', function($scope, $state, $stateParams, Printers) {
+    $scope.printer = Printers.get($stateParams.id)
+  });
+
