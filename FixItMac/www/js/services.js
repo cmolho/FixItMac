acef49bb6ad18512c64ac728b9fb0476e589b515
Ana <amikkels@macalester.edu>
2016-03-07 11:11:48 -0600

    added printer locationCategory, inkStatus, paperStatus, jamStatus, otherStatus

--- FixItMac/www/js/services.js
+++ FixItMac/www/js/services.js
@@ -12,26 +12,61 @@ angular.module('starter.services', [])
     id: 0,
     name: "Dupre",
     location: "Dupre Hall 253",
+    locationCategory: "Dorms",
+    inkStatus: true,
+    paperStatus: true,
+    jamStatus: true,
+    otherStatus: true,
     status: false
+  }, {
+    id: 1,
+    name: "Neil Hall",
+    location: "Neil Hall 015",
+    locationCategory: "Neil Hall",
+    inkStatus: true,
+    paperStatus: true,
+    jamStatus: true,
+    otherStatus: true,
+    status: true
   }, {
     id: 2,
     name: "Campus Center",
     location: "Campus Center 2nd Floor",
+    locationCategory: "Public Spaces",
+    inkStatus: true,
+    paperStatus: true,
+    jamStatus: true,
+    otherStatus: true,
     status: true
-  }, {
+  },{
     id: 3,
     name: "Kirk 1",
     location: "Kirk Hall 012",
+    locationCategory: "Dorms",
+    inkStatus: true,
+    paperStatus: true,
+    jamStatus: true,
+    otherStatus: true,
     status: false
   }, {
     id: 4,
     name: "Kirk 2",
     location: "Kirk Hall 012",
+    locationCategory: "Dorms",
+    inkStatus: true,
+    paperStatus: true,
+    jamStatus: true,
+    otherStatus: true,
     status: false
   }, {
     id: 5,
     name: "Doty",
     location: "Doty Hall 004",
+    locationCategory: "Dorms",
+    inkStatus: true,
+    paperStatus: true,
+    jamStatus: true,
+    otherStatus: true,
     status: true
   }];
 
