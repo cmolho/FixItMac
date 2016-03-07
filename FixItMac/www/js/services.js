e742d51d187228271288926f8bea3bd8b2ae7b2f
gimminju <mkim2@macalester.edu>
2016-03-07 13:23:34 -0600

    some menus added

--- FixItMac/www/js/services.js
+++ FixItMac/www/js/services.js
@@ -12,41 +12,67 @@ angular.module('starter.services', [])
     id: 0,
     name: "Dupre",
     location: "Dupre Hall 253",
+    category: "dorm",
     status: true
   }, {
     id: 2,
     name: "Campus Center",
     location: "Campus Center 2nd Floor",
+    category: "cc",
     status: true
   }, {
     id: 3,
     name: "Kirk 1",
     location: "Kirk Hall 012",
+    category: "dorm",
     status: false
   }, {
     id: 4,
     name: "Kirk 2",
     location: "Kirk Hall 012",
+    category: "dorm",
     status: false
   }, {
     id: 5,
     name: "Doty",
     location: "Doty Hall 004",
+    category: "dorm",
     status: true
   }];
 
   return {
-    all: function() {
+    all: function () {
       return printers;
     },
-    get: function(printerID) {
+    get: function (printerID) {
       for (var i = 0; i < printers.length; i++) {
         if (printers[i].id === parseInt(printerID)) {
           return printers[i];
         }
       }
       return null;
+    },
+    get: function (dorm) {
+      for (var i = 0; i < printers.length; i++) {
+        if (printers.category === "dorm") {
+          return printers;
+        }
+      }
+    },
+    get: function (dorm) {
+      for (var i = 0; i < printers.length; i++) {
+        if (printers.category === "cc") {
+          return printers;
+        }
+        ;
+      }
+    },
+    get: function (dorm) {
+      for (var i = 0; i < printers.length; i++) {
+        if (printers.category === "academic") {
+          return printers;
+        }
+      }
     }
-  };
-
+  }
 });
